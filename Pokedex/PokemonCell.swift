d2a31a726251565786b3130675f31ea4b6e233d4
Shaisan2 <shivam0sharma@icloud.com>
2017-06-20 14:39:49 -0400

    Implemented the methods for CollectionView

--- Pokedex/PokemonCell.swift
+++ Pokedex/PokemonCell.swift
@@ -13,6 +13,12 @@ class PokemonCell: UICollectionViewCell {
     @IBOutlet weak var nameLbl: UILabel!
     
     var pokemon: Pokemon!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
     
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemonCell", for: indexPath) as? PokemonCell {
+            
+            let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
+            
+            cell.configureCell(pokemon: pokemon)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 718
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
     }
 
 }
